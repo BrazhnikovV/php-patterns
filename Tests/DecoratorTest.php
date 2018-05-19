be5801bdae8bdccc67f19733485d37bcbf2005bb
vasek <vasek@valerian>
2018-05-19 14:57:06 +0300

    Правка в тесте декоратора.

--- Tests/DecoratorTest.php
+++ Tests/DecoratorTest.php
@@ -6,6 +6,8 @@ use DesignPatterns\Structural\Decorator;
 use PHPUnit\Framework\TestCase;
 
 require_once('DesignPatterns/Structural/Decorator/Webservice.php');
+require_once('DesignPatterns/Structural/Decorator/JsonRenderer.php');
+require_once('DesignPatterns/Structural/Decorator/XmlRenderer.php');
 
 class DecoratorTest extends TestCase
 {
@@ -17,5 +19,18 @@ class DecoratorTest extends TestCase
     protected function setUp()
     {
         $this->service = new Decorator\Webservice( 'foobar' );
+    }
+
+    public function testJsonDecorator()
+    {
+        $service = new Decorator\JsonRenderer( $this->service );
+        $this->assertEquals( '"foobar"', $service->renderData() );
+    }
+
+    public function testXmlDecorator()
+    {
+        $service = new Decorator\XmlRenderer( $this->service );
+
+        $this->assertXmlStringEqualsXmlString( '<?xml version="1.0"?><content>foobar</content>', $service->renderData() );
     }
 }
